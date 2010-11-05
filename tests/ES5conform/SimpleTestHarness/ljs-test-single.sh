a5aac8e68083ea8c29254fa7bad16f89298b014d
Andrey Chudnov <achudnov@stevens.edu>
2010-11-05 19:00:53 -0400

    Modified the ES5conform test scripts to be more informative and (sic\!) pretty

--- tests/ES5conform/SimpleTestHarness/ljs-test-single.sh
+++ tests/ES5conform/SimpleTestHarness/ljs-test-single.sh
@@ -2,17 +2,27 @@
 
 P=`dirname $0`
 FILE=$1
+F=`basename $FILE`
 output=`mktemp -t ljses5.XXXX`
 errOutput=`mktemp -t ljses5err.XXXX`
 
-$P/../../../src/jsc $P/ljs_harness.js $FILE $P/ljs-run.js -env $P/../../../data/es5-lib.es5 -full-desugar -eval > $output 2> $errOutput
+$P/../../../build/jsc.native $P/ljs_harness.js $FILE $P/ljs-run.js -env $P/../../../data/es5-lib.es5 -full-desugar -eval > $output 2> $errOutput
 
 failed1=`grep Failed $output`
 failed2=`grep failed $output`
 failed3=`grep exception $errOutput`
 failed="$failed1$failed2$failed3"
+
 if [[ $failed != "" ]]; 
 then
+    #we failed this test
+    echo -e "$F [\e[0;31mFAILED\e[0m]"
     exit 1;
+fi
+#we passed this test
+if [[ $2 == "-full-output" ]];
+then 
+    #we want to output the passed test cases as well
+    echo -e "$F [\e[0;32mPASSED\e[0m]"
 fi
 exit 0;
