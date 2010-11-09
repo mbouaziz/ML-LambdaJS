b0e1378363984257490d5f4e40bee5ab48ead793
Mehdi Bouaziz <mehdi@umeb>
2010-11-09 14:39:58 -0500

    Removed ES3 from the branch master = ES5

--- src/jsc.ml
+++ src/jsc.ml
@@ -1,16 +1,5 @@
 open JavaScript
-open Exprjs
 open Prelude
-open Printf
-open Format
-open Lambdajs_cps
-open Exprjs_syntax
-open Format
-open Lambdajs_lattice
-open Lambdajs_env
-open Lambdajs_syntax
-open Lambdajs_eval
-open Lexing
 module ES5 = Es5
 module ES5s = Es5_syntax
 module ES5ds = Es5_desugar
@@ -20,9 +9,8 @@ module ES5p = Es5_parser
 module ES5eval = Es5_eval
 module ES5v = Es5_values
 
-module H = Hashtbl
 
-let p = (Lexing.dummy_pos, Lexing.dummy_pos)
+let p = Lexing.dummy_pos, Lexing.dummy_pos
 
 let env_special_id = "**ENV-SPECIAL-ID**"
 let env_special_node = ES5s.EOp1(p, `Prim1 env_special_id, ES5s.EConst (p, JavaScript_syntax.CUndefined))
@@ -44,41 +32,21 @@ let srcexp = function
   | PrimExp e -> prim_to_src e
   | _ -> assert false
 
-let srcLJS = ref (EConst (p, JavaScript_syntax.CUndefined))
 let srcES5 = ref Nothing
-let lang = ref "es5"
 
-let action_set_lang (lang_in : string) : unit = 
-  match lang_in with
-    | "ljs" -> lang := "ljs"
-    | "es5" -> lang := "es5"
-    | _ -> failwith ("unknown language: " ^ lang_in)
-     
- 
 let xopen_in path = if path = "STDIN" then stdin else open_in path
 
 
 let load_js (path : string) : unit = 
   let js = parse_javascript_from_channel (xopen_in path) path in
-    match !lang with
-      |	"ljs" ->
-	  srcLJS := 
-	    ESeq (p, !srcLJS,
-		  Lambdajs_syntax.desugar (Exprjs_syntax.from_javascript js))
-      | "es5" ->
-	  let newES5 = ES5ds.ds_top (Exprjs_syntax.from_javascript js) in
-	  srcES5 := begin match !srcES5 with
-	  | Nothing -> SrcExp newES5
-	  | PrimEnv _ -> failwith "Env applied to nothing followed by a JS file"
-	  | SrcExp e -> SrcExp (ES5s.ESeq (p, e, newES5))
-	  | PrimExp e -> SrcExp (ES5s.ESeq (p, prim_to_src e, newES5))
-	  end
-      | _ -> failwith ("Unknown language: " ^ !lang)
+  let newES5 = ES5ds.ds_top (Exprjs_syntax.from_javascript js) in
+  srcES5 := match !srcES5 with
+  | Nothing -> SrcExp newES5
+  | PrimEnv _ -> failwith "Env applied to nothing followed by a JS file"
+  | SrcExp e -> SrcExp (ES5s.ESeq (p, e, newES5))
+  | PrimExp e -> SrcExp (ES5s.ESeq (p, prim_to_src e, newES5))
 
-let load_lambdajs (path : string) : unit =
-  srcLJS := ESeq (p, !srcLJS,
-		  Lambdajs.parse_lambdajs (xopen_in path) path)
-    
+
 let load_es5 (path : string) : unit =
   let parsed = ES5.parse_es5 (xopen_in path) path in
   srcES5 := match !srcES5 with
@@ -87,26 +55,23 @@ let load_es5 (path : string) : unit =
   | SrcExp e -> SrcExp (ES5s.ESeq (p, e, prim_to_src parsed))
   | PrimExp e -> PrimExp (ES5s.ESeq (p, e, parsed))
 
+
 let load_file (path : string) : unit =
   if Filename.check_suffix path ".js" then
     load_js path
-  else if Filename.check_suffix path ".jsl" then
-    load_lambdajs path
   else if Filename.check_suffix path ".es5" then
     load_es5 path
   else 
-    failwith ("unknown file extention; try -js or -jsl")
+    failwith "unknown file extention; try -js or -ljs"
+
 
 let desugar () : unit =
-  match !lang with
-    | "ljs" -> srcLJS := Lambdajs_desugar.desugar_op !srcLJS
-    | "es5" -> srcES5 := begin match !srcES5 with
-      | Nothing -> failwith "Nothing to desugar"
-      | PrimEnv e -> PrimEnv (ES5ds.desugar (prim_to_src e))
-      | SrcExp e -> PrimExp (ES5ds.desugar e)
-      | PrimExp e -> PrimExp (ES5ds.desugar (prim_to_src e))
-      end
-    | _ -> failwith ("Unknown language: " ^ !lang)
+  srcES5 := match !srcES5 with
+  | Nothing -> failwith "Nothing to desugar"
+  | PrimEnv e -> PrimEnv (ES5ds.desugar (prim_to_src e))
+  | SrcExp e -> PrimExp (ES5ds.desugar e)
+  | PrimExp e -> PrimExp (ES5ds.desugar (prim_to_src e))
+
 
 let apply_env env x = match x with
   | Nothing -> PrimEnv env
@@ -114,79 +79,57 @@ let apply_env env x = match x with
   | SrcExp e -> SrcExp (ES5s.substitute env_special_node e (prim_to_src env))
   | PrimExp e -> PrimExp (ES5s.substitute env_special_node e env)
 
+
 let set_env (s : string) =
-  match !lang with
-    | "ljs" -> srcLJS := enclose_in_env (parse_env (xopen_in s) s) !srcLJS
-    | "es5" -> srcES5 := apply_env (ES5e.parse_env (xopen_in s) s env_special_node) !srcES5
-    | _ -> failwith ("Unknown language: " ^ !lang)
+  srcES5 := apply_env (ES5e.parse_env (xopen_in s) s env_special_node) !srcES5
+
 
 let read_env_from_cache_file (s : string) =
-  match !lang with
-  | "ljs" -> failwith ("Read env from cache not implemented for " ^ !lang)
-  | "es5" ->
-      let cin = open_in_bin s in
-      let cached = Marshal.from_channel cin in
-      let _ = close_in cin in
-      srcES5 := begin match !srcES5, cached with
-      | _, Nothing -> failwith ("Nothing in file " ^ s)
-      | Nothing, _ -> cached
-      | x, PrimEnv env -> apply_env env x
-      | SrcExp e1, SrcExp e2 -> SrcExp (ES5s.ESeq (p, e1, e2))
-      | SrcExp e1, PrimExp e2 -> SrcExp (ES5s.ESeq (p, e1, prim_to_src e2))
-      | PrimExp e1, SrcExp e2 -> SrcExp (ES5s.ESeq (p, prim_to_src e1, e2))
-      | PrimExp e1, PrimExp e2 -> PrimExp (ES5s.ESeq (p, e1, e2))
-      | PrimEnv _, _ -> failwith "Unapplied environment followed by code"
-      end
-  | _ -> failwith ("Unknown language: " ^ !lang)
+  let cin = open_in_bin s in
+  let cached = Marshal.from_channel cin in
+  let _ = close_in cin in
+  srcES5 := match !srcES5, cached with
+  | _, Nothing -> failwith ("Nothing in file " ^ s)
+  | Nothing, _ -> cached
+  | x, PrimEnv env -> apply_env env x
+  | SrcExp e1, SrcExp e2 -> SrcExp (ES5s.ESeq (p, e1, e2))
+  | SrcExp e1, PrimExp e2 -> SrcExp (ES5s.ESeq (p, e1, prim_to_src e2))
+  | PrimExp e1, SrcExp e2 -> SrcExp (ES5s.ESeq (p, prim_to_src e1, e2))
+  | PrimExp e1, PrimExp e2 -> PrimExp (ES5s.ESeq (p, e1, e2))
+  | PrimEnv _, _ -> failwith "Unapplied environment followed by code"
+
 
 let write_env_to_cache_file (s : string) =
-  match !lang with
-  | "ljs" -> failwith ("Writing env to cache not implemented for " ^ !lang)
-  | "es5" ->
-      let cout = open_out_bin s in
-      Marshal.to_channel cout !srcES5 [Marshal.Closures];
-      close_out cout
-  | _ -> failwith ("Unknown language: " ^ !lang)
+  let cout = open_out_bin s in
+  Marshal.to_channel cout !srcES5 [Marshal.Closures];
+  close_out cout
+
 
 let action_pretty () : unit =
-  match !lang with
-    | "ljs" -> failwith ("Pretty not implemented for " ^ !lang)
-    | "es5" -> Es5_pretty.exp (srcexp !srcES5) std_formatter;
-	print_newline ()
-    | _ -> failwith ("Unknown language: " ^ !lang)
+  Es5_pretty.exp (srcexp !srcES5) Format.std_formatter;
+  print_newline ()
 
-let action_cps () : unit =
-  let cpslambdajs = Lambdajs_cps.cps !srcLJS in
-    Lambdajs_cps.p_cpsexp cpslambdajs std_formatter
 
 let action_eval () : unit =
-  match !lang with
-    | "es5" -> ignore (ES5eval.eval_expr (primexp !srcES5)); print_newline ()
-    | _ -> failwith ("Not implemented for language: " ^ !lang)
+  ignore (ES5eval.eval_expr (primexp !srcES5));
+  print_newline ()
 
-let action_operators () : unit =
-  let ops = operators !srcLJS in
-    IdSet.p_set text ops std_formatter;
-    print_newline ()
-      
+
 let action = ref (fun () -> ())
 
 let set_action (thunk : unit -> unit) (() : unit) : unit =
   let prev = !action in
   action :=  fun () -> prev (); thunk ()
 
+
 let main () : unit =
   Arg.parse
     [ 
-      ("-lang", Arg.String action_set_lang,
-       "(ljs|es5) select the language to use.  Not all operations work with
-all languages");
-
       ("-js", Arg.String load_js,
        "<file> Load <file> as JavaScript");
 
-      ("-jsl", Arg.String load_lambdajs,
-       "Load <file> as LambdaJS");
+      ("-ljs", Arg.String load_es5,
+       "Load <file> as LambdaJS-ES5");
 
       ("-env", Arg.String set_env,
       "<file> load <file> as environment");
@@ -198,12 +141,6 @@ all languages");
        "<file> write <file> as a cached environment");
 
       ("-full-desugar", Arg.Unit (set_action desugar), "like it says");
-
-      ("-operators", Arg.Unit (set_action action_operators),
-       "list operators used");
-
-       ("-cps", Arg.Unit (set_action action_cps),
-       "convert program to CPS");
 
        ("-eval", Arg.Unit (set_action action_eval),
 	"run the program");
@@ -221,5 +158,5 @@ let _ =
       print_endline (Printexc.to_string e);
       Printexc.print_backtrace stdout
   in
-  pp_print_flush std_formatter ();
-  pp_print_flush err_formatter ()
+  Format.pp_print_flush Format.std_formatter ();
+  Format.pp_print_flush Format.err_formatter ()
