ad2198cb6dcd652a19f282c9798fcbadb9c9fda5
Mehdi Bouaziz <mehdi@umeb>
2010-12-01 16:44:31 -0500

    Fixed the 'arguments' bug

--- src/desugar_helpers.ml
+++ src/desugar_helpers.ml
@@ -41,13 +41,20 @@ let mk_array (p, exps) =
 		      :: List.map2 mk_num_field (iota (List.length exps)) exps)) }
 
 (* 10.6 *)
-let args_obj ?(from_parser=false) p arg_list =
+let args_obj ?(from_parser=false) ?(strict_mode=false) p arg_list =
   let mk_field n v = (string_of_int n,  mk_val p v) in
+  let attrs =
+    [("proto", obj_proto ~from_parser p);
+     ("class", str p "Arguments");
+     ("extensible", if strict_mode then false_c p else true_c p)]
+      (* Need true here because of function body desugaring. Turned into false into every desugared function *)
+  in
+  let attrs = if strict_mode then attrs else ("configurable", true_c p)::attrs in
+  (* Need configurable to remove fields *)
   { p ; e = EObject 
       (* 10.6 steps 4, 6 *)
-      ([("proto", obj_proto ~from_parser p);
-	("class", str p "Arguments");
-	("extensible", false_c p)],
+      (
+	attrs,
        (* 10.6 steps 1, 7 *)
        (("length", [(Value, int_c p (List.length arg_list));
 		    (Writable, true_c p);
@@ -67,37 +74,12 @@ let args_obj ?(from_parser=false) p arg_list =
 
 (* Used by getters and setters---the function will be known at
 runtime *)
-let args_thunk ?(from_parser=false) p arg_list =
-  { p ; e = ELambda (["func"], args_obj ~from_parser p arg_list) }
+let args_thunk ?(from_parser=false) ?(strict_mode=false) p arg_list =
+  { p ; e = ELambda (["func"], args_obj ~from_parser ~strict_mode p arg_list) }
 
-
-(* Same idea as in original \JS --- use the args array *)
-let func_expr_lambda ?(from_parser=false) p ids body =
-  let folder id ix e =
-    { p ; e = ELet (
-	id,
-	{ p ; e = EGetFieldSurface (
-	    { p ; e = EId "arguments" },
-	    { p ; e = EConst (S.CString (string_of_int ix)) },
-	    args_thunk ~from_parser p []) },
-	e) } in
-    { p ; e = ELambda (
-	["this"; "arguments"],
-	List.fold_right2 folder ids (iota (List.length ids)) body) }
-
-
-(* Small extension to allow a named function to call itself.  I have
-   not considered the repercussions of functions with the same name as
-   an argument. *)
-
-let func_stmt_lambda p func_name ids body = func_expr_lambda p ids body
-(*  ELet (p, 
-	func_name, 
-	EId (p, "$funobj"),
-	func_expr_lambda p ids body) *)
     
 (* 13.2 *)
-let func_object ?(from_parser=false) p ids lambda_exp =
+let func_object ?(from_parser=false) ?(strict_mode=false) p ids lambda_exp =
   { p ; e = ELet (
       "$prototype",
       { p ; e = EObject (
@@ -130,7 +112,7 @@ let func_object ?(from_parser=false) p ids lambda_exp =
 			      { p ; e = EId "$prototype" },
 			      { p ; e = EConst (S.CString "constructor") },
 			      { p ; e = EId "$funobj" },
-			      args_thunk ~from_parser p [{ p ; e = EId "$funobj" }]) },
+			      args_thunk ~from_parser ~strict_mode p [{ p ; e = EId "$funobj" }]) },
 			  { p ; e = EId "$funobj"}) }) }) }
 
 let new_obj p proto_id = 
@@ -139,3 +121,51 @@ let new_obj p proto_id =
        ("extensible", true_c p);
        ("class", str p "Object")],
       []) }
+
+
+(* Same idea as in original \JS --- use the args array *)
+let func_expr_lambda ?(from_parser=false) ?(strict_mode=false) p ids body =
+  let e_args = { p ; e = EId "arguments" } in
+  let folder id ix e =
+    let e_six = { p ; e = EConst (S.CString (string_of_int ix)) } in
+    { p ; e = ELet (
+	id,
+	{ p ; e = EGetFieldSurface (e_args, e_six, args_thunk ~from_parser ~strict_mode p []) },
+	if strict_mode then
+	  e
+	else
+	(* partial "arguments"-bug fix:
+	   instead of just let x = arguments[0] in
+	   we remove
+	   arguments[0]
+	   and add it again as an accessor, the getter returns x, the setter sets x
+	*)
+	  let e_getter = func_object ~from_parser ~strict_mode p [] { p ; e = ELambda (["this"; "arguments"], { p ; e = EId id }) } in
+	  let e_setter = func_object ~from_parser ~strict_mode p [""] { p ; e = ELambda (["this"; "arguments"], { p ; e = ESet(id, { p ; e = EGetFieldSurface(e_args, { p ; e = EConst (S.CString "0") }, { p ; e = EConst S.CUndefined }) }) }) } in
+	  { p ; e = ESeq (
+	      { p ; e = ESeq (
+		  { p ; e = EDeleteField (e_args, e_six) },
+		  { p ; e = ESeq (
+		      { p ; e = ESetAttr (Getter, e_args, e_six, e_getter) },
+		      { p ; e = ESetAttr (Setter, e_args, e_six, e_setter) }) }) },
+	      e) }
+      ) } in
+  let body = if strict_mode then
+    body
+  else
+    { p ; e = ESeq ({ p ; e = EOp1(`Prim1 "prevent-extensions", e_args) }, body) }
+  in
+  { p ; e = ELambda (
+      ["this"; "arguments"],
+      List.fold_right2 folder ids (iota (List.length ids)) body) }
+
+
+(* Small extension to allow a named function to call itself.  I have
+   not considered the repercussions of functions with the same name as
+   an argument. *)
+
+let func_stmt_lambda p func_name ids body = func_expr_lambda p ids body
+(*  ELet (p,
+	func_name,
+	EId (p, "$funobj"),
+	func_expr_lambda p ids body) *)
