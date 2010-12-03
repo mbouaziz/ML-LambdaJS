9c7fb9e2defaca9a811f60e3b0591be492a25971
Mehdi Bouaziz <mehdi@umeb>
2010-12-02 21:17:09 -0500

    [semi-colon insertion] Use these new parser & lexer. No test broken

--- src/javaScript.ml
+++ src/javaScript.ml
@@ -2,18 +2,21 @@ open Prelude
 open JavaScript_syntax
 open Lexing
 
+module JSL = JavaScriptSCI_lexer
+module JSP = JavaScriptSCI_parser
+
 let parse_javascript cin name =
   let lexbuf = Lexing.from_string cin in
     try 
       (* Set the correct filename in lexbuf (for source-tracking). *)
       lexbuf.lex_curr_p <- { lexbuf.lex_curr_p with pos_fname = name };
-      JavaScript_parser.program JavaScript_lexer.token lexbuf
+      JSP.program JSL.token lexbuf
     with
       |  Failure "lexing: empty token" ->
            failwith (sprintf "lexical error at %s"
                        (string_of_position 
                           (lexbuf.lex_curr_p, lexbuf.lex_curr_p)))
-      | JavaScript_parser.Error ->
+      | JSP.Error ->
            failwith (sprintf "parse error at %s; unexpected token %s"
                        (string_of_position 
                           (lexbuf.lex_curr_p, lexbuf.lex_curr_p))
@@ -24,13 +27,13 @@ let parse_javascript_from_channel cin name =
     try 
       (* Set the correct filename in lexbuf (for source-tracking). *)
       lexbuf.lex_curr_p <- { lexbuf.lex_curr_p with pos_fname = name };
-      JavaScript_parser.program JavaScript_lexer.token lexbuf
+      JSP.program JSL.token lexbuf
     with
       |  Failure "lexing: empty token" ->
            failwith (sprintf "lexical error at %s"
                        (string_of_position 
                           (lexbuf.lex_curr_p, lexbuf.lex_curr_p)))
-      | JavaScript_parser.Error ->
+      | JSP.Error ->
            failwith (sprintf "parse error at %s; unexpected token %s"
                        (string_of_position 
                           (lexbuf.lex_curr_p, lexbuf.lex_curr_p))
@@ -40,13 +43,13 @@ let parse_expr cin name =
   let lexbuf = Lexing.from_string cin in
     try 
       lexbuf.lex_curr_p <- { lexbuf.lex_curr_p with pos_fname = name };
-      JavaScript_parser.expression JavaScript_lexer.token lexbuf
+      JSP.expression JSL.token lexbuf
     with
       |  Failure "lexing: empty token" ->
            failwith (sprintf "lexical error at %s"
                        (string_of_position 
                           (lexbuf.lex_curr_p, lexbuf.lex_curr_p)))
-      | JavaScript_parser.Error ->
+      | JSP.Error ->
            failwith (sprintf "parse error at %s"
                        (string_of_position 
                           (lexbuf.lex_curr_p, lexbuf.lex_curr_p)))
